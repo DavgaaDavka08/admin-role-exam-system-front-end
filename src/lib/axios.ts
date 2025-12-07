083bcaee28eab547c9e27edcd5c91196be67d030
DavgaaDavka08 <iderbatdagvadorj@gmail.com>
2025-12-07 18:54:20 +0800

    done

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
+   baseURL: "https://admin-role-exam-systems-backend.onrender.com/api",
   withCredentials: true,
 });
 
